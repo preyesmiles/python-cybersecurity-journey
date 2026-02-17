e75b3d3f8c8879a4722a9ad36ab8807d1189207f
Samuel Kris-Edonmi <krispreye@gmail.com>
2026-02-17 14:52:52 +0100

    made changes to printAndVariables.py

--- PrintAndVariablesLesson/printAndVariables.py
+++ PrintAndVariablesLesson/printAndVariables.py
@@ -1,12 +1,12 @@
 #This block of code demonstrates the use of both input and print function to print out a full name
-#firstName = input("Enter your first name: ")
-#lastName = input("Enter your last name: ")
-#print("Your name is " + firstName + " " + lastName)
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last name: ")
+print("Your name is " + firstName + " " + lastName)
 
 #This block of code demonstrates the print and input functions and also the use of concatenation.
 print("Welcome New user")
 print("Your new twitter handle and bio " + " " + ("@cyber"+input("What is your pets name: ")) + " " + "From" + " " + input("Whats the name of the city you where born in: ") )
 
 #This block of code demonstrates the print statement using the f formart which evaluates variables or expressions inside cuerly braces{}
-#name = "smiles"
-#print(f"My name is {name}!")
+name = "smiles"
+print(f"My name is {name}!")
