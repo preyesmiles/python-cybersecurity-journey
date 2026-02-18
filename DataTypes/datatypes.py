b42015eea9f184c9e0e460ea8bf790c78e39d289
Samuel Kris-Edonmi <krispreye@gmail.com>
2026-02-18 22:32:30 +0100

    Update datatypes.py code and add snapshots of code outputs to DataTypes folder

---
+++ DataTypes/datatypes.py
@@ -0,0 +1,15 @@
+# Data Types
+# 1 strings
+# Shows how to output the kind of data being handled with the function "type"
+print(type("Hello World"))
+# Shows how to index a string
+print("Hello World" [4])
+
+# 2 integer
+print(type(5))
+
+# 3 Float
+print(type(5.67))
+
+# Boolean
+print(type(True or False))
